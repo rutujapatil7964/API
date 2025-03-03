cb8c71922f1829c29564a38852809fdb0ebc3fd8
Rutuja Patil <rutujapatil7964@gmail.com>
2025-03-03 12:33:36 +0530

    removed unnecessary lines

--- models/Student.js
+++ models/Student.js
@@ -10,17 +10,6 @@ const StudentSchema = new mongoose.Schema({
   course: { type: String, required: true },
 });
 
-// studentSchema.pre("save", async function (next) {
-//   if (!this.roll_no) {
-//     const lastStudent = await this.constructor.findOne().sort("-roll_no"); // Get last roll_no
-//     this.roll_no = lastStudent ? lastStudent.roll_no + 1 : 0; // Start from 1
-//   }
-//   next();
-// });
-
-
-//export default mongoose.model("Student", StudentSchema);
-
 const Student = mongoose.model("Student", StudentSchema)
 
 export default Student
